0d5e8623c6cb05bcaff3f10a28263f1ba78aa97e
h3iino <tatanih3@gmail.com>
2021-05-30 19:31:01 +0900

    a

--- coco_cnn.py
+++ coco_cnn.py
@@ -3,5 +3,6 @@ from pycocotools.coco import COCO
 anno_path = "coco/annotations/instances_train2014.json"
 coco = COCO(anno_path)
 
+# try
 print(coco.getCatIds(catNms=["dog", "cat"]))
 print(coco.getCatIds(supNms=["vehicle"]))
