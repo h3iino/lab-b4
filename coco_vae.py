b48434dc6c4e06c59c1711be0d91582c1e85cedf
h3iino <tatanih3@gmail.com>
2021-06-11 16:16:53 +0900

    a

--- coco_vae.py
+++ coco_vae.py
@@ -17,7 +17,7 @@ class Coco_Dataset(torch.utils.data.Dataset):
         # 指定する場合は前処理クラスを受け取る
         self.transform = transform[data_kind]
         # label: 80種類
-        # self.category_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+        self.category_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
         # 画像を読み込むファイルパスとラベルのリスト
         self.images = []
         self.labels = []
@@ -85,9 +85,8 @@ class CNN_vae(nn.Module):
 
     def __init__(self):
         super(CNN_vae, self).__init__()
-        
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        
+
         self.Encoder = nn.Sequential(  # in(3*256*256)
             nn.Conv2d(3, 16, kernel_size=11, stride=4, padding=5),  # out(16*64*64)
             nn.ReLU(inplace=True),
@@ -104,16 +103,17 @@ class CNN_vae(nn.Module):
             nn.ConvTranspose2d(16, 3, kernel_size=4, stride=4),  # out(3*256*256)
             nn.ReLU(inplace=True),
         )
+
         self.fc1 = nn.Linear(512, 256)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 512)
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=11, stride=4, padding=5)  # out(16*64*64)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # out(16*32*32)
-        self.conv2 = nn.Conv2d(16, 8, kernel_size=5, stride=2, padding=2)  # out(8*16*16)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # out(8*8*8)
+        # self.conv1 = nn.Conv2d(3, 16, kernel_size=11, stride=4, padding=5)  # out(16*64*64)
+        # self.relu1 = nn.ReLU(inplace=True)
+        # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # out(16*32*32)
+        # self.conv2 = nn.Conv2d(16, 8, kernel_size=5, stride=2, padding=2)  # out(8*16*16)
+        # self.relu2 = nn.ReLU(inplace=True)
+        # self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # out(8*8*8)
 
         # self.t_conv1 = nn.ConvTranspose2d(8, 8, kernel_size=2, stride=2)  # out(8*16*16)
         # self.relu3 = nn.ReLU(inplace=True)
@@ -122,38 +122,30 @@ class CNN_vae(nn.Module):
         # self.t_conv3 = nn.ConvTranspose2d(16, 3, kernel_size=4, stride=4)  # out(3*256*256)
         # self.relu5 = nn.ReLU(inplace=True)
 
-    def sample_z(self, x_mean, x_var):  # latent variable
+    def sample_z(self, x_mean, x_var):
         epsilon = torch.randn(x_mean.shape).to(self.device)
         return x_mean + torch.sqrt(x_var) * epsilon
-
+    
     def KL_divergence(self, mean, var):
-        return -0.5 * torch.mean(torch.sum(1 + torch.log(var) - mean**2 - var))
-        # return 0.5 * torch.sum(mean**2 + var - torch.log(var) - 1)
+        # return -0.5 * torch.mean(torch.sum(1 + torch.log(var) - mean**2 - var))
+        return -0.5 * torch.sum(1 + torch.log(var) - mean**2 - var)
+
+    def reconstruction_loss(self, x, x_decode):
+        return torch.mean(torch.sum((x - x_decode) ** 2, axis=1))
 
     def forward(self, x):
-        # print(x.shape)
-        x = self.conv1(x)
-        # print(x.shape)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-        # x = self.Encoder(x)
-        # print("----")
-        
-        x_mean = torch.flatten(x, 1)  # 512dim
-        x_var = torch.flatten(x, 1)  # 512dim
-        x_mean = self.fc1(x_mean)
-        x_var = self.fc2(x_var)
+        x_encode = self.Encoder(x)
 
+        x_mean = torch.flatten(x_encode, 1)
+        x_var = torch.flatten(x_encode, 1)
         z = self.sample_z(x_mean, x_var)
-        z = self.fc3(z)
         z = z.reshape(z.size()[0], 8, 8, 8)
-        # print("z: ", z.size())
-        # kl = self.KL_divergence(x_mean, x_var)
 
-        x = self.Decoder(z)
+        x_decode = self.Decoder(z)
+
+        kld = self.KL_divergence(x_mean, x_var)
+        rec_loss = self.reconstruction_loss(x, x_decode)
+        loss = kld + rec_loss
 
         # x = self.conv1(x)
         # x = self.relu1(x)
@@ -169,7 +161,7 @@ class CNN_vae(nn.Module):
         # x = self.t_conv3(x)
         # x = self.relu5(x)
 
-        return x, x_mean, x_var
+        return x, loss
 
 
 def training(train_loader, model, criterion, optimizer, device, model_flag):
@@ -180,11 +172,11 @@ def training(train_loader, model, criterion, optimizer, device, model_flag):
         images = images.to(device)
         
         model.zero_grad()
-        outputs, mean, var = model(images)
-
-        mse = criterion(outputs, images)
-        kl = model.KL_divergence(mean, var)
-        loss = kl + mse
+        if model_flag == "linear":
+            images = images.reshape(-1, 3*256*256)
+        outputs, loss = model(images)
+        
+        # loss = criterion(outputs, images)
         loss.backward()
         optimizer.step()
 
@@ -205,11 +197,8 @@ def testing(test_loader, model, criterion, optimizer, device, model_flag):
 
         if model_flag == "linear":
             images = images.reshape(-1, 3*256*256)
-        outputs, mean, var = model(images)
-        
-        mse = criterion(outputs, images)
-        kl = model.KL_divergence(mean, var)
-        loss = kl + mse
+        outputs, loss = model(images)
+        # loss = criterion(outputs, images)
         val_loss += loss.item()
         # val_acc += (outputs.max(1)[1] == labels).sum().item()  #
         outputs_and_inputs.append((outputs, images))
@@ -228,7 +217,7 @@ def drawing_graph(num_epoch, train_loss_list, val_loss_list, draw_flag="loss"):
     plt.ylabel('loss')
     plt.title('Training and validation ' + draw_flag)
     plt.grid()
-    loss_fig.savefig(path + "coco_VAE_" + draw_flag + "_0607.png")
+    loss_fig.savefig(path + "coco_VAE_" + draw_flag + "_0612.png")
     plt.show()
 
 # Min-Maxスケーリング
@@ -254,12 +243,12 @@ def show_image(img, image_flag):
     npimg = img.detach().numpy()
     figure_image = plt.figure()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    figure_image.savefig(path + "coco_VAE_" + image_flag + "_0607.png")
+    figure_image.savefig(path + "coco_VAE_" + image_flag + "_0612.png")
     plt.show()
 
 def main():
-    num_epoch = 30
-    num_batch = 32
+    num_epoch = 20
+    num_batch = 64
     data_train_num = 2000
     data_val_num = 500
     data_test_num = 500
@@ -308,8 +297,10 @@ def main():
                                                 shuffle=False, num_workers=2)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = CNN_vae().to(device)
-
+    if model_flag == "cnn":
+        model = CNN_vae().to(device)
+    else:
+        model = Linear_AutoEncoder().to(device)
     print(device)  # GPUを使えているか
     print(model)  # ネットワーク構造を記述
 
@@ -351,8 +342,6 @@ def main():
     if is_save == True:
         model_path = 'model_vae_' + str(epoch+1) + '.pth'
         optim_path = 'optim_vae_' + str(epoch+1) + '.pth'
-        # model_path = 'model_ae.pth'
-        # optim_path = 'optim_ae.pth'
         torch.save(model.state_dict(), model_path)
         torch.save(optimizer.state_dict(), optim_path)
 
