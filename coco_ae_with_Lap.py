4875042750e67777360f98e22afbd68c755d871c
h3iino <tatanih3@gmail.com>
2021-06-15 20:02:51 +0900

    a

--- coco_ae_with_Lap.py
+++ coco_ae_with_Lap.py
@@ -184,8 +184,8 @@ def make_edge(images):
 def laploss(output_image, input_image, criterion):
     output_edge = make_edge(output_image)
     input_edge = make_edge(input_image)
-    output_edge = output_edge.to('cpu')  # ---
-    show_image(output_edge.reshape(-1, 3, 256, 256), image_flag="in")  # ---
+    # output_edge = output_edge.to('cpu')  # ---
+    # show_image(output_edge.reshape(-1, 3, 256, 256), image_flag="--")  # ---
     loss = criterion(output_edge, input_edge)
     return loss
 
