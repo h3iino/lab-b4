fb5e85d587d8728b89184488ea46bb91c4eb509e
h3iino <tatanih3@gmail.com>
2021-06-15 16:59:04 +0900

    a

--- coco_ae_with_Lap.py
+++ coco_ae_with_Lap.py
@@ -192,8 +192,10 @@ def training(train_loader, model, criterion, optimizer, device):
         # loss = criterion(outputs, images)
         # loss_r64 = criterion2(r64_outputs, resize64_images)
         # loss_r16 = criterion3(r16_outputs, resize16_images)
-        # loss = loss + loss_r64 + loss_r16
         loss = laploss(outputs, images, criterion)
+        loss_r64 = laploss(r64_outputs, resize64_images)
+        loss_r16 = laploss(r16_outputs, resize16_images)
+        loss = loss + loss_r64 + loss_r16
 
         loss.backward()
         optimizer.step()
@@ -220,8 +222,10 @@ def testing(test_loader, model, criterion, optimizer, device):
         # loss = criterion(outputs, images)
         # loss_r64 = criterion2(r64_outputs, resize64_images)
         # loss_r16 = criterion3(r16_outputs, resize16_images)
-        # loss = loss + loss_r64 + loss_r16
         loss = laploss(outputs, images, criterion)
+        loss_r64 = laploss(r64_outputs, resize64_images)
+        loss_r16 = laploss(r16_outputs, resize16_images)
+        loss = loss + loss_r64 + loss_r16
 
         val_loss += loss.item()
         # val_acc += (outputs.max(1)[1] == labels).sum().item()  #
