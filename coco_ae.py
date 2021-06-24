320ca509d1212f71f0084796f05bfabc29bc7abd
h3iino <tatanih3@gmail.com>
2021-06-24 16:37:12 +0900

    a

--- coco_ae.py
+++ coco_ae.py
@@ -97,7 +97,7 @@ class CNN_AutoEncoder(nn.Module):
         super(CNN_AutoEncoder, self).__init__()
         self.Encoder = nn.Sequential(  # in(3*256*256)
             nn.Conv2d(3, 32, kernel_size=11, stride=4, padding=5),  # out(64*64*64)
-            nn.BatchNorm2d(32),
+            # nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=5, stride=2, padding=2),  # out(32*32*32)
             nn.ReLU(inplace=True),
@@ -111,7 +111,7 @@ class CNN_AutoEncoder(nn.Module):
             nn.ConvTranspose2d(8, 8, kernel_size=2, stride=2),  # out(16*16*16)
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(8, 16, kernel_size=2, stride=2),  # out(16*32*32)
-            nn.BatchNorm2d(16),
+            # nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(16, 32, kernel_size=2, stride=2),  # out(16*64*64)
             nn.ReLU(inplace=True),
