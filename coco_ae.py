c4f080a64a6a8335fa9ed57f590e0d069578a0ed
h3iino <tatanih3@gmail.com>
2021-06-24 21:02:58 +0900

    a

--- coco_ae.py
+++ coco_ae.py
@@ -96,13 +96,13 @@ class CNN_AutoEncoder(nn.Module):
     def __init__(self):
         super(CNN_AutoEncoder, self).__init__()
         self.Encoder = nn.Sequential(  # in(3*256*256)
-            nn.Conv2d(3, 32, kernel_size=11, stride=4, padding=5),  # out(64*64*64)
-            nn.BatchNorm2d(32),
+            nn.Conv2d(3, 16, kernel_size=11, stride=4, padding=5),  # out(64*64*64)
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=5, stride=2, padding=2),  # out(32*32*32)
-            nn.BatchNorm2d(32),
+            nn.Conv2d(16, 16, kernel_size=5, stride=2, padding=2),  # out(32*32*32)
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 16, kernel_size=5, stride=2, padding=2),  # out(16*16*16)
+            nn.Conv2d(16, 16, kernel_size=5, stride=2, padding=2),  # out(16*16*16)
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Conv2d(16, 8, kernel_size=5, stride=2, padding=2),  # out(16*8*8)
@@ -116,13 +116,13 @@ class CNN_AutoEncoder(nn.Module):
             nn.ConvTranspose2d(8, 16, kernel_size=2, stride=2),  # out(16*32*32)
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(16, 32, kernel_size=2, stride=2),  # out(16*64*64)
-            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(16, 16, kernel_size=2, stride=2),  # out(16*64*64)
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2),  # out(16*128*128)
-            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(16, 16, kernel_size=2, stride=2),  # out(16*128*128)
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(32, 3, kernel_size=2, stride=2),  # out(3*256*256)
+            nn.ConvTranspose2d(16, 3, kernel_size=2, stride=2),  # out(3*256*256)
             nn.BatchNorm2d(3),
             # nn.ReLU(inplace=True),
             nn.Tanh(),
