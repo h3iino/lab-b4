0f5bd2d54e7b104759f2924343111af35acbe3f2
h3iino <tatanih3@gmail.com>
2021-07-02 17:00:05 +0900

    a

--- coco_ae.py
+++ coco_ae.py
@@ -268,7 +268,7 @@ def show_image(img, image_flag):
 def main():
     num_epoch = 100
     # num_batch = 128
-    num_batch = 10
+    num_batch = 5
     data_train_num = 200
     data_val_num = 50
     data_test_num = 50
