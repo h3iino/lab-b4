70a36fb80df62bd7cb34d88106293f2a9d02c26d
h3iino <tatanih3@gmail.com>
2021-07-03 20:27:31 +0900

    a

--- coco_ae_with_Lap_sample.py
+++ coco_ae_with_Lap_sample.py
@@ -114,17 +114,17 @@ class CNN_AutoEncoder(nn.Module):
             nn.Tanh(),
         )
         self.Decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(8, 8, kernel_size=2, stride=2),  # out(8*16*16)
+            nn.ConvTranspose2d(8, 64, kernel_size=2, stride=2),  # out(8*16*16)
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=4),  # out(3*64*64)
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=4),  # out(3*64*64)
             # nn.ReLU(inplace=True),
             # nn.Sigmoid(),
             nn.Tanh(),
         )
         self.Decoder3 = nn.Sequential(
-            nn.ConvTranspose2d(8, 8, kernel_size=2, stride=2),  # out(8*16*16)
+            nn.ConvTranspose2d(8, 64, kernel_size=2, stride=2),  # out(8*16*16)
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(8, 3, kernel_size=2, stride=2),  # out(3*32*32)
+            nn.ConvTranspose2d(64, 3, kernel_size=2, stride=2),  # out(3*32*32)
             # nn.ReLU(inplace=True),
             # nn.Sigmoid(),
             nn.Tanh(),
