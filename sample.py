173e0f49e323af32bc4ad5e8e36777577a8457a8
h3iino <tatanih3@gmail.com>
2021-07-05 03:04:44 +0900

    a

--- sample.py
+++ sample.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import numpy as np
 
-im_in = np.array(Image.open('movies/coco_AutoEncoder_in_sample_0702.jpg'))
-im_out = np.array(Image.open('movies/coco_AutoEncoder_out_sample_0702.jpg'))
+im_in = np.array(Image.open('movies/coco_AutoEncoder_in_sample_0702.png'))
+im_out = np.array(Image.open('movies/coco_AutoEncoder_out_sample_0702.png'))
 
 tmp = im_in**2 - im_out**2
 
