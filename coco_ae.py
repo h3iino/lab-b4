a9b30f2edf698177c4825952f2534e6da93f0182
h3iino <tatanih3@gmail.com>
2021-07-04 18:54:50 +0900

    a

--- coco_ae.py
+++ coco_ae.py
@@ -174,15 +174,15 @@ class CNN_AutoEncoder(nn.Module):
 
         # x = x.reshape(-1, 512)
         # mid_x = enc_x.view(-1, 512)
-        mid_x = enc_x.reshape(batch_size, -1)
-        # print("e", enc_x.shape)
-        # print("m", mid_x.shape)
+        mid_x = enc_x.view(batch_size, -1)
+        print("e", enc_x.dtype)
+        print("m", mid_x.dtype)
         # x = self.fc(x)
         mid_x = self.rl1(self.bn1(self.fc1(mid_x)))
         mid_x = self.rl2(self.bn2(self.fc2(mid_x)))
         # mid_x = self.rl3(self.bn3(self.fc3(mid_x)))
         dec_x = mid_x.reshape(batch_size, 2, 16, 16)
-        # print("d", dec_x.shape)
+        print("d", dec_x.dtype)
 
         dec_x = self.Decoder(dec_x)
 
