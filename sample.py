db6cd84fdc799e64aeb2d028438e5c1d25f9e09c
h3iino <tatanih3@gmail.com>
2021-07-05 03:26:36 +0900

    a

--- sample.py
+++ sample.py
@@ -24,6 +24,6 @@ loss = criterion(outputs, images)
 print(loss)
 
 np.set_printoptions(threshold=np.inf)
-print(im_in[100:110])
+print(im_in[100:110][100:110])
 print("------------")
-print(im_out[100:110])
+print(im_out[100:110][100:110])
