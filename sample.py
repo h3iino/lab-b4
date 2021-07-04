bdcc3fe0c51dee242da5537b8df500c648d83b4e
h3iino <tatanih3@gmail.com>
2021-07-05 03:08:04 +0900

    a

--- sample.py
+++ sample.py
@@ -1,11 +1,17 @@
 from PIL import Image
 import numpy as np
+import torch
 
 im_in = np.array(Image.open('movies/coco_AutoEncoder_in_sample_0702.png'))
 im_out = np.array(Image.open('movies/coco_AutoEncoder_out_sample_0702.png'))
 
-tmp = im_in**2 - im_out**2
+im_in_t = torch.Tensor(im_in)
+im_out_t = torch.Tensor(im_out)
+
+# tmp = im_in**2 - im_out**2
+tmp = im_in_t**2 - im_out_t**2
 
 tmp2 = np.average(tmp)
+tmp2 = torch.average(tmp)
 
 print(tmp2)
